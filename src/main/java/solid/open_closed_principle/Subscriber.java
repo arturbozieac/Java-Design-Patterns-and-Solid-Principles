373499998e83c6a07238377e9f80526d173cd915
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-14 20:19:42 +0200

    SOLID - Open-Close principle #2
    Extract common class components to a generic class

--- src/main/java/solid/open_closed_principle/Subscriber.java
+++ src/main/java/solid/open_closed_principle/Subscriber.java
@@ -1,6 +1,75 @@
 package solid.open_closed_principle;
 
+public abstract class Subscriber {
 
-public class Subscriber {
+	protected Long subscriberId;
 
+	protected String address;
+
+	protected Long phoneNumber;
+
+	protected int baseRate;
+
+	/**
+	 * Calculates the bill for the subscriber.
+	 * 
+	 * @return The bill value.
+	 */
+	public abstract double calculateBill();
+
+	/**
+	 * @return the subscriberId
+	 */
+	public Long getSubscriberId() {
+		return subscriberId;
+	}
+
+	/**
+	 * @param subscriberId the subscriberId to set
+	 */
+	public void setSubscriberId(Long subscriberId) {
+		this.subscriberId = subscriberId;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public Long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(Long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the baseRate
+	 */
+	public int getBaseRate() {
+		return baseRate;
+	}
+
+	/**
+	 * @param baseRate the baseRate to set
+	 */
+	public void setBaseRate(int baseRate) {
+		this.baseRate = baseRate;
+	}
 }
