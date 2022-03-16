236a2c4511e4c542a3e730313744a48f567150a9
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-16 21:23:35 +0200

    Creational Patterns - Prototype start

---
+++ src/main/java/creationalPatterns/prototype/Client.java
@@ -0,0 +1,10 @@
+package creationalPatterns.prototype;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+        
+	}
+
+}
---
+++ src/main/java/creationalPatterns/prototype/Swordsman.java
@@ -0,0 +1,17 @@
+package creationalPatterns.prototype;
+
+
+public class Swordsman extends GameUnit {
+
+	private String state = "idle";
+
+	public void attack() {
+		this.state = "attacking";
+	}
+
+	@Override
+	public String toString() {
+		return "Swordsman "+state+" @ "+getPosition();
+	}
+
+}
