7c2aba0c4db8d082f3fc13ebfe67d3c5f5fbff84
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-21 20:59:01 +0200

    Creational Patterns - Singleton start

---
+++ src/main/java/creationalPatterns/singleton/Client.java
@@ -0,0 +1,9 @@
+package creationalPatterns.singleton;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/creationalPatterns/singleton/EagerRegistry.java
@@ -0,0 +1,9 @@
+package creationalPatterns.singleton;
+
+/**
+ * This class uses eager initialization of singleton instance.
+ */
+public class EagerRegistry {
+	
+
+}
