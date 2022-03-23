71e70a159476b235d04eaa4ee8f65760af130481
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-23 21:34:49 +0200

    Structural Patterns - Adapter implementations - Object Adapter/Class Adapter

--- src/main/java/structuralPatterns/adapter/EmployeeClassAdapter.java
+++ src/main/java/structuralPatterns/adapter/EmployeeClassAdapter.java
@@ -4,6 +4,21 @@ package structuralPatterns.adapter;
 /**
  * A class adapter, works as Two-way adapter
  */
-public class EmployeeClassAdapter {
+public class EmployeeClassAdapter extends Employee implements Customer{
+
+	@Override
+	public String getName() {
+		return this.getFullName();
+	}
+
+	@Override
+	public String getDesignation() {
+		return this.getJobTitle();
+	}
+
+	@Override
+	public String getAddress() {
+		return this.getOfficeLocation();
+	}
 	
 }
--- src/main/java/structuralPatterns/adapter/Main.java
+++ src/main/java/structuralPatterns/adapter/Main.java
@@ -5,10 +5,19 @@ public class Main {
 
 	public static void main(String[] args) {
 		/** Using Class/Two-way adapter **/ 
+		EmployeeClassAdapter adapter1 = new EmployeeClassAdapter();
+		populateEmployeeData(adapter1);
 		
+		BusinessCardDesigner designer = new BusinessCardDesigner();
+		String card = designer.designCard(adapter1);
+		System.out.println(card);
 		
 		/** Using Object Adapter **/
-		
+		Employee employee = new Employee();
+		populateEmployeeData(employee);
+		EmployeeObjectAdapter adapter2 = new EmployeeObjectAdapter(employee);
+		card = designer.designCard(adapter2);
+		System.out.println(card);
 	}
 
 	private static void populateEmployeeData(Employee employee) {
