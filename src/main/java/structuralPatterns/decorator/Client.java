a661f160511a078e1941b1ec255a33803f807b6e
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-28 21:23:55 +0300

    Structural Patterns - Decorator implementation

--- src/main/java/structuralPatterns/decorator/Client.java
+++ src/main/java/structuralPatterns/decorator/Client.java
@@ -5,6 +5,12 @@ public class Client {
 	public static void main(String[] args) {
 		Message m = new TextMessage("The <FORCE> is strong with this one!");
 		System.out.println(m.getContent());
+		
+		m = new HtmlEncodedMessage(m);
+		System.out.println(m.getContent());
+		
+		m = new Base64EncodedMessage(m);
+		System.out.println(m.getContent());
 
 	}
 }
--- src/main/java/structuralPatterns/decorator/HtmlEncodedMessage.java
+++ src/main/java/structuralPatterns/decorator/HtmlEncodedMessage.java
@@ -1,6 +1,20 @@
 package structuralPatterns.decorator;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 //Decorator. Implements component interface
-public class HtmlEncodedMessage {
+public class HtmlEncodedMessage implements Message {
+
+	private Message message;
+	
+	public HtmlEncodedMessage(Message msg) {
+		this.message = msg;
+	}
+	
+	@Override
+	public String getContent() {
+		// Deprecated, used only for example.
+		return StringEscapeUtils.escapeHtml4(message.getContent());
+	}
 
 }
