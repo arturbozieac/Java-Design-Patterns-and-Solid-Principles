40073f7fc349856ffd32df637a40776e1b01a444
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-28 20:47:49 +0300

    Structural Patterns - Bridge implementation

--- src/main/java/structuralPatterns/bridge/Client.java
+++ src/main/java/structuralPatterns/bridge/Client.java
@@ -4,7 +4,16 @@ package structuralPatterns.bridge;
 public class Client {
 
 	public static void main(String[] args) {
+		FifoCollection<Integer> collection = new Queue<>(new SinglyLinkedList<>());
+		collection.offer(10);
+		collection.offer(20);
+		collection.offer(30);
 		
+		System.out.println(collection.poll());
+		System.out.println(collection.poll());
+		System.out.println(collection.poll());
+		
+		System.out.println(collection.poll());
 	}
 
 }
--- src/main/java/structuralPatterns/bridge/Queue.java
+++ src/main/java/structuralPatterns/bridge/Queue.java
@@ -1,7 +1,27 @@
 package structuralPatterns.bridge;
 
 //A refined abstraction.
-public class Queue<T> {
+public class Queue<T> implements FifoCollection<T>{
+	 /**
+	  * The list that stores our queue objects.
+	  */
+	private LinkedList<T> list;
+
+	public Queue(LinkedList<T> list) {
+		this.list = list;
+	}
+	
+	@Override
+	public void offer(T element) {
+		list.addFirst(element);
+	}
+
+	@Override
+	public T poll() {
+		return list.removeFirst();
+	}
+	
+	
 	
 	
 }
