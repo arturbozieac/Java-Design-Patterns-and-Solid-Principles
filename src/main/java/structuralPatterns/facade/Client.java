5739f7ae1702a7888497775bba27094c6894d66d
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-03-29 19:09:06 +0300

    Structural Patterns - Facade implementation

--- src/main/java/structuralPatterns/facade/Client.java
+++ src/main/java/structuralPatterns/facade/Client.java
@@ -1,28 +1,14 @@
 package structuralPatterns.facade;
 
-import structuralPatterns.facade.Template.TemplateType;
 
 public class Client {
 
 	public static void main(String[] args) {
 		Order order = new Order("101", 99.99);
-		
-		boolean result = sendOrderEmailWithoutFacade(order);
+		EmailFacade facade = new EmailFacade();
+		boolean result = facade.sendOrderEmail(order);
 		
 		System.out.println("Order Email "+ (result?"sent!":"NOT sent..."));
 		
 	}
-
-	private static boolean sendOrderEmailWithoutFacade(Order order) {
-		Template template = TemplateFactory.createTemplateFor(TemplateType.Email);
-		Stationary stationary = StationaryFactory.createStationary();
-		Email email = Email.getBuilder()
-					  .withTemplate(template)
-					  .withStationary(stationary)
-					  .forObject(order)
-					  .build();
-		Mailer mailer = Mailer.getMailer();
-		return mailer.send(email);
-	}
-	
 }
--- src/main/java/structuralPatterns/facade/EmailFacade.java
+++ src/main/java/structuralPatterns/facade/EmailFacade.java
@@ -1,7 +1,19 @@
 package structuralPatterns.facade;
 
+import structuralPatterns.facade.Template.TemplateType;
 
 //Facade provides simple methods for client to use
 public class EmailFacade {
 	
+	public boolean sendOrderEmail(Order order) {
+		Template template = TemplateFactory.createTemplateFor(TemplateType.Email);
+		Stationary stationary = StationaryFactory.createStationary();
+		Email email = Email.getBuilder()
+					  .withTemplate(template)
+					  .withStationary(stationary)
+					  .forObject(order)
+					  .build();
+		Mailer mailer = Mailer.getMailer();
+		return mailer.send(email);
+	}
 }
