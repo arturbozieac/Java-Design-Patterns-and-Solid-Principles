a8e83cd65f07366d93aa648e480678c860331022
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-04-05 21:04:57 +0300

    Behavioral Patterns - Interpreter implementation

--- src/main/java/behavioralPatterns/interpreter/Client.java
+++ src/main/java/behavioralPatterns/interpreter/Client.java
@@ -4,7 +4,14 @@ package behavioralPatterns.interpreter;
 public class Client {
 
 	public static void main(String[] args) {
+		Report report = new Report("My report", "NOT ADMIN OR USER AND FINANCE_ADMIN");
+		ExpressionBuilder builder = new ExpressionBuilder();
 		
+		PermissionExpression exp = builder.build(report);
+		System.out.println(exp);
+		
+		User user = new User("Gary", "FINANCE_ADMIN");
+		System.out.println(exp.interpret(user));
 	}
 
 }
