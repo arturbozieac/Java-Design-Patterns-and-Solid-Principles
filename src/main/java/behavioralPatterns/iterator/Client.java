656252c2a1d466a40e834539a5f62d06b3d54c38
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-04-06 20:45:47 +0300

    Behavioral Patterns - Iterator implementation

--- src/main/java/behavioralPatterns/iterator/Client.java
+++ src/main/java/behavioralPatterns/iterator/Client.java
@@ -1,9 +1,12 @@
-package com.coffeepoweredcrew.iterator;
+package behavioralPatterns.iterator;
 
 public class Client {
 
 	public static void main(String[] args) {
-
+		Iterator<ThemeColor> it = ThemeColor.getIterator();
+		while(it.hasNext()) {
+			System.out.println(it.next());
+		}
 	}
 
 }
