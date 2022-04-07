cf34646b6d4a3c81923c1d1c345711045470e898
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-04-07 20:26:54 +0300

    Behavioral Patterns - Template method implementation

--- src/main/java/behavioralPatterns/templateMethod/Client.java
+++ src/main/java/behavioralPatterns/templateMethod/Client.java
@@ -1,8 +1,23 @@
 package behavioralPatterns.templateMethod;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Client {
 
 	public static void main(String[] args) {
+		Order order = new Order("2023");
 
+		order.addItem("Soda", 2.50);
+		order.addItem("Sandwitch", 11.95);
+		order.addItem("Pizza", 15.95);
+
+		OrderPrinter printer = new HtmlPrinter();
+		File orderFile = new File("OrderReport.html");
+		try {
+			printer.printOrder(order, orderFile.getName());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/behavioralPatterns/templateMethod/OrderPrinter.java
+++ src/main/java/behavioralPatterns/templateMethod/OrderPrinter.java
@@ -1,7 +1,28 @@
 package behavioralPatterns.templateMethod;
 
-//Abstract base class defines the template method
-public class OrderPrinter {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+//Abstract base class defines the template method
+public abstract class OrderPrinter {
+
+	public void printOrder(Order order, String fileName) throws FileNotFoundException {
+		try(PrintWriter writer = new PrintWriter(fileName)){
+			writer.println(start());
+			writer.println(formatOrderNumber(order));
+			writer.println(formatItems(order));
+			writer.println(formatTotal(order));
+			writer.println(end());			
+		}
+	}
 	
+	protected abstract String start();
+	
+	protected abstract String formatOrderNumber(Order order);
+
+	protected abstract String formatItems(Order order);
+
+	protected abstract String formatTotal(Order order);
+
+	protected abstract String end();
 }
