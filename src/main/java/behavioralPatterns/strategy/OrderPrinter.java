2cef684f56e17911272dd258ec26f42a61d5b423
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-04-07 19:55:43 +0300

    Behavioral Patterns - Strategy start

---
+++ src/main/java/behavioralPatterns/strategy/OrderPrinter.java
@@ -0,0 +1,8 @@
+package behavioralPatterns.strategy;
+
+import java.util.Collection;
+
+//Strategy
+public interface OrderPrinter {
+
+}
---
+++ src/main/java/behavioralPatterns/strategy/SummaryPrinter.java
@@ -0,0 +1,9 @@
+package behavioralPatterns.strategy;
+
+import java.util.Collection;
+
+//Concrete strategy
+public class SummaryPrinter {
+
+	
+}
