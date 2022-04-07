2c268dff89dc8b2d03518d2bd6d2b70fd236fc28
Bozîeac Artur <39851804+arturbozieac@users.noreply.github.com>
2022-04-07 20:12:31 +0300

    Behavioral Patterns - Strategy implementation

--- src/main/java/behavioralPatterns/strategy/SummaryPrinter.java
+++ src/main/java/behavioralPatterns/strategy/SummaryPrinter.java
@@ -1,9 +1,23 @@
 package behavioralPatterns.strategy;
 
 import java.util.Collection;
+import java.util.Iterator;
 
 //Concrete strategy
-public class SummaryPrinter {
+public class SummaryPrinter implements OrderPrinter{
+
+	@Override
+	public void print(Collection<Order> orders) {
+		System.out.println("************* Sumarry Report ***********");
+        Iterator<Order> iter = orders.iterator();
+        double total = 0;
+        for(int i=1;iter.hasNext();i++) {
+            Order order = iter.next();
+            System.out.println(i+". "+order.getId()+" \t"+order.getDate());
+        }
+        System.out.println("----------------------------------------");
+        System.out.println("\tGrand Total "+total);
+	}
 
 	
 }
